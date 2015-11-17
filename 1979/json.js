2406cf2db8c4959f5b4d7ce5590b5dfdaf0febe3
Niels G. W. Serup <ngws@metanohi.name>
2015-11-17 13:24:22 +0100

    Tilføj 1979 ordentligt.

---
+++ 1979/json.js
@@ -0,0 +1,180 @@
+{
+   "name" : "DIKUrevy",
+   "year" : "1979",
+   "acts" : [
+      {
+         "materials" : [
+            {
+               "revuename" : "DIKUrevy",
+               "instructors" : [],
+               "roles" : [
+                  {
+                     "title" : "Sanger 1 - sopran",
+                     "abbr" : "S1",
+                     "actor" : ""
+                  },
+                  {
+                     "title" : "Sanger 2 - alt",
+                     "actor" : "",
+                     "abbr" : "S2"
+                  },
+                  {
+                     "title" : "Sanger 3 - tenor",
+                     "abbr" : "S2",
+                     "actor" : ""
+                  },
+                  {
+                     "actor" : "",
+                     "abbr" : "S2",
+                     "title" : "Sanger 4 - bas"
+                  }
+               ],
+               "length" : 0,
+               "revueyear" : "1979",
+               "type" : "sketch",
+               "location" : "sange/aabningsnummer.tex",
+               "status" : "Færdig",
+               "author" : "QLM og HVO",
+               "order" : 1,
+               "version" : "1.0",
+               "props" : [],
+               "title" : "Åbningsnummer"
+            },
+            {
+               "roles" : [
+                  {
+                     "actor" : "",
+                     "abbr" : "S1",
+                     "title" : "Sanger 1"
+                  }
+               ],
+               "length" : 0,
+               "revuename" : "DIKUrevy",
+               "instructors" : [],
+               "melody" : "Magge-duddi og jeg",
+               "revueyear" : "1979",
+               "order" : 2,
+               "author" : "CH",
+               "location" : "sange/DIKUGuiden.tex",
+               "status" : "Færdig",
+               "type" : "song",
+               "composer" : "?",
+               "title" : "DIKU-Guiden",
+               "props" : [],
+               "version" : "1.0"
+            },
+            {
+               "melody" : "Poul sine høns i haven lod flyve",
+               "revueyear" : "1979",
+               "length" : 0,
+               "roles" : [
+                  {
+                     "title" : "Sanger 1",
+                     "actor" : "L",
+                     "abbr" : "S1"
+                  },
+                  {
+                     "title" : "Sanger 2",
+                     "actor" : "H",
+                     "abbr" : "S2"
+                  },
+                  {
+                     "title" : "Sanger 3",
+                     "actor" : "J",
+                     "abbr" : "S2"
+                  },
+                  {
+                     "title" : "Sanger 4",
+                     "actor" : "B",
+                     "abbr" : "S2"
+                  }
+               ],
+               "revuename" : "DIKUrevy",
+               "instructors" : [],
+               "title" : "Sangen om Søren",
+               "props" : [],
+               "composer" : "?",
+               "version" : "1.0",
+               "location" : "sange/SangenOmSoeren.tex",
+               "status" : "Færdig",
+               "author" : "QL",
+               "order" : 3,
+               "type" : "song"
+            },
+            {
+               "order" : 4,
+               "author" : "APR, OC, KK",
+               "location" : "sange/Sprogforbistring.tex",
+               "status" : "Færdig",
+               "type" : "song",
+               "composer" : "?",
+               "props" : [
+                  {
+                     "description" : "",
+                     "name" : "USA-trøje",
+                     "responsible" : ""
+                  },
+                  {
+                     "description" : "",
+                     "name" : "Reel til bandet",
+                     "responsible" : "Bandet"
+                  }
+               ],
+               "title" : "Sprogforbistring",
+               "version" : "1.0",
+               "length" : 0,
+               "roles" : [
+                  {
+                     "title" : "Sanger 1",
+                     "actor" : "A",
+                     "abbr" : "S1"
+                  },
+                  {
+                     "actor" : "B",
+                     "abbr" : "S1",
+                     "title" : "Sanger 2"
+                  }
+               ],
+               "instructors" : [],
+               "revuename" : "DIKUrevy",
+               "melody" : "I say tomatoes...",
+               "revueyear" : "1979"
+            },
+            {
+               "version" : "1.0",
+               "title" : "Rapportopgavesketch",
+               "props" : [],
+               "type" : "sketch",
+               "order" : 5,
+               "author" : "BOK, TH, HVO",
+               "status" : "Færdig",
+               "location" : "sketches/Rapportopgavesketch.tex",
+               "revueyear" : "1979",
+               "instructors" : [],
+               "revuename" : "DIKUrevy",
+               "length" : 0,
+               "roles" : [
+                  {
+                     "title" : "Studerende 1",
+                     "actor" : "",
+                     "abbr" : "y"
+                  },
+                  {
+                     "title" : "Studerende 2",
+                     "actor" : "",
+                     "abbr" : "x"
+                  },
+                  {
+                     "title" : "Studerende 3",
+                     "abbr" : "z",
+                     "actor" : ""
+                  }
+               ]
+            }
+         ],
+         "title" : "Akt 1",
+         "order" : 1,
+         "length" : 0
+      }
+   ]
+}
