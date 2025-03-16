d2d8493a7deafa6e642b76e0e6714636456cef73
Jonatan <jonatanruizm@gmail.com>
2025-03-16 23:06:00 +0100

    Long anticipated

---
+++ 2023/.config.mk
@@ -0,0 +1,34 @@
+# Makefile configuration for RevyScript
+#
+# Author: munter@diku.dk
+#
+# This file should be copied and adapted to your local environment
+
+# Define where the scripts are placed.
+scriptdir := /home/jonatan/Desktop/DIKUREVY2023/RevyTeX/scripts
+
+# The directories where original .tex materials will be placed
+sketchdir := sketches
+songdir := sange
+videodir := video
+bitsdir := brokker
+contactsdir := kontakter
+
+# The acts plan. A cleartext file with act headings and material order
+plan := aktoversigt.plan
+
+# Define where to place output files
+outputdir := www
+
+# Where to put the datafile
+json := $(outputdir)/json.js
+
+# Auto generated PDF output placements
+manus := $(outputdir)/Manuskript.pdf
+acts := $(outputdir)/Aktoversigt.pdf
+roles := $(outputdir)/Rolleoversigt.pdf
+signup := $(outputdir)/Rolletilmelding.pdf
+props := $(outputdir)/Rekvisitliste.pdf
+songs := $(outputdir)/Sangmanuskript.pdf
+contacts := $(outputdir)/Kontaktliste.pdf
+individualdir := $(outputdir)/Individuelt
